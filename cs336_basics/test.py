7c03043664be67fe1486103c63ad9ce348e7aeec
Zhongqi Ren <rzq23@mails.tsinghua.edu.cn>
2026-03-12 19:30:41 +0800

    updated test.py

--- cs336_basics/test.py
+++ cs336_basics/test.py
@@ -12,14 +12,21 @@ from collections import Counter
 counter = Counter()
 counter["apple"] = 2
 counter["banana"] += 1
+# counter["apple"] -= 2
+del counter["apple"]
+del counter["orange"]
+counter["apple"] = 1
+counter2 = Counter({"apple": 3, "pie": 4})
 print(counter)
-print(counter.items())
+# print(counter.items())
+print(counter2)
+print(counter - counter2)
 # a = tuple("仁".encode("utf-8"))
 # b = "仁".encode("utf-8")
 # print(a)
 # print(b)
 # # print(bytes([11])+bytes([12]))
-# # for byte in 
+# # for byte in
 # print(a[0])
 # print(type(b[0]))
 # print(type(a[0]))
