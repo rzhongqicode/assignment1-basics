de088a6095612bdb066a546b5787c2a2142a50c6
wsl <rzq23@mails.tsinghua.edu.cn>
2026-03-12 12:04:19 +0800

    add test.py

---
+++ cs336_basics/test.py
@@ -0,0 +1,25 @@
+from collections import Counter
+
+# a = "abc".encode("utf-8")
+# b = "def".encode("utf-8")
+# print(a)
+# print(b)
+# print(a<b)
+# print(a+b)
+# a = [1,2,3]
+# print(a[:-1])
+
+counter = Counter()
+counter["apple"] = 2
+counter["banana"] += 1
+print(counter)
+print(counter.items())
+# a = tuple("仁".encode("utf-8"))
+# b = "仁".encode("utf-8")
+# print(a)
+# print(b)
+# # print(bytes([11])+bytes([12]))
+# # for byte in 
+# print(a[0])
+# print(type(b[0]))
+# print(type(a[0]))
