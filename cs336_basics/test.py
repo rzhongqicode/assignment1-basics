5bb5117bebb8f34c701bd3503cdc84d981ea5d2d
Zhongqi Ren <rzq23@mails.tsinghua.edu.cn>
2026-04-12 13:57:47 +0800

    change test.py

--- cs336_basics/test.py
+++ cs336_basics/test.py
@@ -1,4 +1,7 @@
+import math
 from collections import Counter
+
+import torch
 
 # a = "abc".encode("utf-8")
 # b = "def".encode("utf-8")
@@ -9,18 +12,18 @@ from collections import Counter
 # a = [1,2,3]
 # print(a[:-1])
 
-counter = Counter()
-counter["apple"] = 2
-counter["banana"] += 1
-# counter["apple"] -= 2
-del counter["apple"]
-del counter["orange"]
-counter["apple"] = 1
-counter2 = Counter({"apple": 3, "pie": 4})
-print(counter)
-# print(counter.items())
-print(counter2)
-print(counter - counter2)
+# counter = Counter()
+# counter["apple"] = 2
+# counter["banana"] += 1
+# # counter["apple"] -= 2
+# del counter["apple"]
+# del counter["orange"]
+# counter["apple"] = 1
+# counter2 = Counter({"apple": 3, "pie": 4})
+# print(counter)
+# # print(counter.items())
+# print(counter2)
+# print(counter - counter2)
 # a = tuple("仁".encode("utf-8"))
 # b = "仁".encode("utf-8")
 # print(a)
@@ -30,3 +33,10 @@ print(counter - counter2)
 # print(a[0])
 # print(type(b[0]))
 # print(type(a[0]))
+# a = bytes()
+# print(a)
+# a += b"a"
+# print(a)
+# a += b"b"
+# print(a)
+print(math.pi)
