bef6a1cf266563fc45bd58bddc992230e201f44c
wsl <rzq23@mails.tsinghua.edu.cn>
2026-04-13 23:09:34 +0800

    refined prepare_data.py

--- cs336_basics/prepare_data.py
+++ cs336_basics/prepare_data.py
@@ -1,18 +1,26 @@
 from cs336_basics.tokenizer import Tokenizer
 import numpy as np
+from tqdm import tqdm
 
 tokenizer = Tokenizer.from_files(
     vocab_filepath="tiny_vocab.json", 
     merges_filepath="tiny_merges.txt", 
     special_tokens=["<|endoftext|>"])
 
-input_file_path = "data/TinyStoriesV2-GPT4-train.txt"
+input_file_path = "data/TinyStoriesV2-GPT4-valid.txt"
+token_ids_list = []
 with open(input_file_path, mode="r", encoding="utf-8") as f:
-    text_data = f.read()
-
-token_ids_list = tokenizer.encode(text=text_data)
-
+    total_lines = sum(1 for _ in f)
+    f.seek(0)
+    # process line by line
+    for line in tqdm(f, total=total_lines, desc="Encoding Validation Data"):
+        temp = tokenizer.encode(text=line)
+        token_ids_list.extend(temp)
+        
+# token_ids_list = tokenizer.encode(text=text_data)
+print("converting to numpy array and saving...")
 arr = np.array(token_ids_list, dtype=np.uint16)
 
-output_file_path = "tiny_train_data.npy"
+output_file_path = "tiny_val_data.npy"
 np.save(output_file_path, arr)
+print(f"dataset process finished, total {len(arr)} tokens")
